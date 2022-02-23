18ae04e93d81173313965b30a279bf9f44ee28ef
Omid Aiene <omid.aienad@gmail.com>
2022-02-23 11:04:23 +0330

    update #5 implementing add-sign-class.directive 14001204

--- src/app/shared/components/led/Directives/add-sign-class.directive.ts
+++ src/app/shared/components/led/Directives/add-sign-class.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[mapAddSignClass]'
 })
 export class AddSignClassDirective {
 
-  constructor() { }
+  constructor(private render2: Renderer2, private elementRef: ElementRef<any>) { }
+
+  @Input("mapAddSignClass") set addSign(index:number) {
+
+    const cssClass:string = `h${index + 1}`;
+
+    this.render2.addClass(this.elementRef.nativeElement, cssClass);
+
+  }
 
 }
